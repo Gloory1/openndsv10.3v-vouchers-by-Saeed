a51cab1a13f8d046649c821c5306073ca11749a5
Gloory1 <167514139+Gloory1@users.noreply.github.com>
2025-06-25 01:23:27 +0300

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Check and install required packages
+opkg update
+opkg install opennds block-mount kmod-usb-storage kmod-fs-ext4 kmod-fs-vfat kmod-fs-ntfs usbutils
+
+# Make sure USB is mounted at /mnt/usb
+mkdir -p /mnt/usb
+mount -o rw /dev/sda1 /mnt/usb
+
+# Create log directory and voucher file on USB
+mkdir -p /mnt/usb/ndslog
+touch /mnt/usb/ndslog/vouchers.txt
+
+# Download theme_voucher.sh from GitHub repository
+# TODO: add theme_voucer_by_SaeedMuhammed
+wget -O /usr/lib/opennds/theme_voucher.sh https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/theme_voucher.sh
+chmod +x /usr/lib/opennds/theme_voucher.sh
+
+# Download logo image to correct location
+mkdir -p /etc/opennds/htdocs/images
+wget -O /etc/opennds/htdocs/images/logo.png "https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/logo.png"
+
+# Configure openNDS
+uci set opennds.@opennds[0].enabled='1'
+uci set opennds.@opennds[0].gatewayinterface='br-lan'
+uci set opennds.@opennds[0].login_option_enabled='3'
+uci set opennds.@opennds[0].themespec_path='/usr/lib/opennds/theme_voucher.sh'
+uci set opennds.@opennds[0].log_mountpoint='/mnt/usb'
+uci set opennds.@opennds[0].preauthidletimeout='10'
+uci set opennds.@opennds[0].authidletimeout='60'
+uci set opennds.@opennds[0].sessiontimeout='360'
+uci set opennds.@opennds[0].checkinterval='15'
+uci add_list opennds.@opennds[0].trustedmac='d8:43:ae:3d:1a:ee'
+uci commit opennds
+
+# Done
+echo "Setup complete. Reboot the router or restart opennds to apply changes."
