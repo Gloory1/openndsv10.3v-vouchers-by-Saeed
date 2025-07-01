ab448fc5f58597b0846e8d3bd5401eb7535275c2
Gloory1 <167514139+Gloory1@users.noreply.github.com>
2025-07-01 23:56:51 +0300

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -40,7 +40,7 @@ sleep 3
 
 # Download logo image to the correct location
 mkdir -p /etc/opennds/htdocs/images
-wget -O /etc/opennds/htdocs/images/splash.jpg "https://raw.githubusercontent.com/Gloory1/openndsv10.3v-vouchers-by-Saeed/059ef23863922fee52f37de7dc13a29d2e4817f4/etc/opennds/htdocs/images/splash.jpg"
+wget -O /etc/opennds/htdocs/images/splash.jpg "https://raw.githubusercontent.com/Gloory1/openndsv10.3v-vouchers-by-Saeed/059ef23863922fee52f37de7dc13a29d2e4817f4/etc/opennds/htdocs/splash.jpg"
 echo "⏳ Waiting for 3 seconds..."
 sleep 3
 
