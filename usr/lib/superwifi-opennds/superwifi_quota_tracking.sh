701771854834a1d46724d479c6b04768e22fd826
Gloory1 <167514139+Gloory1@users.noreply.github.com>
2025-10-31 20:50:18 +0200

    Update superwifi_quota_tracking.sh vs
    new version v2

--- usr/lib/superwifi-opennds/superwifi_quota_tracking.sh
+++ usr/lib/superwifi-opennds/superwifi_quota_tracking.sh
@@ -4,34 +4,19 @@
 #
 # Include sql manager lib
 #
-. /usr/lib/superwifi/superwifi_database_lib.sh
+. /usr/lib/superwifi/superwifi_database_manager.sh
 #------------------------------------------------------------------------
-
-update_accum_auto() {
-  local raw="$1"
-  local download_this_season="$2"
-
-  # Init database lib
-
-  if echo "$raw" | grep -iq "preemptivemac-"; then
-    local decoded=$(printf '%b' "${raw//%/\\x}" | tr -d '\n' | tr -d '\r')
-    local mac=$(echo "$decoded" | sed 's/.*preemptivemac-//I' | tr 'A-Z' 'a-z')
-    local token=$(get_last_voucher_for_mac "$mac")
-    update_accum "$token" "$download_this_season"
-  else
-    local token=$(echo "$raw" | base64 -d 2>/dev/null)
-    [ -z "$token" ] && return
-    update_accum "$token" "$download_this_season"
-  fi
-}
 
 # JSON from openNDS
 ndsctl json | jq -r '
   .clients | to_entries[] |
-  "\(.value.custom) \(.value.download_this_session) \(.value.upload_this_session)"
-' | while read -r custom download_this_session upload_this_session; do
+  "\(.value.mac) \(.value.upload_this_session) \(.value.download_this_session)"
+' | while read -r mac upload_this_session download_this_session; do
 
-  total_session_usage=$((download_this_session + upload_this_session))
-  update_accum_auto "$custom" "$total_session_usage"
+  finished=$(update_accumulated_usage_by_mac "$mac" "$upload_this_session" "$download_this_session")
+
+  if [ "$finished" -eq 1 ]; then
+    ndsctl deauth "$mac"
+  fi
 
 done
