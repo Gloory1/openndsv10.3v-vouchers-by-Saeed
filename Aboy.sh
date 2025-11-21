384cc9739d9e097f675c7a11e4e56de5a0b24e2e
Gloory1 <167514139+Gloory1@users.noreply.github.com>
2025-11-21 17:07:41 +0200

    Update Aboy.sh

--- Aboy.sh
+++ Aboy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-uci add_list opennds.@opennds[0].fas_custom_variables_list="provider_name=أبو يوســف"
+uci add_list opennds.@opennds[0].fas_custom_variables_list="provider_name=النيل كافيه"
 uci commit opennds
